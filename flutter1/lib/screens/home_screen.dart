58064bbf822e98aea751c0999d3c10a8014b924c
Nurdan <nurdan37pehlivan@gmail.com>
2024-07-21 15:44:55 +0300

    modified

--- flutter1/lib/screens/home_screen.dart
+++ flutter1/lib/screens/home_screen.dart
@@ -23,9 +23,12 @@ class _HomeScreenState extends State<HomeScreen> {
     final User? user = FirebaseAuth.instance.currentUser;
 
     List<Widget> _widgetOptions = <Widget>[
-      HomeScreenContent(),
+      HomeScreenContent(user: user), // Kullanıcıyı geçiyoruz
       const CategoriesPage(), // Kategoriler sayfasını buraya ekleyin
-      const Center(child: Text('Ayarlar Ekranı', style: TextStyle(color: Colors.white))), // Diğer sayfalar için örnek içerik
+      const Center(
+          child: Text('Ayarlar Ekranı',
+              style: TextStyle(
+                  color: Colors.white))), // Diğer sayfalar için örnek içerik
     ];
 
     return Scaffold(
@@ -42,7 +45,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      backgroundColor: Colors.black,
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -68,12 +70,12 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class HomeScreenContent extends StatelessWidget {
-  const HomeScreenContent({super.key});
+  final User? user;
+
+  const HomeScreenContent({super.key, this.user});
 
   @override
   Widget build(BuildContext context) {
-    final User? user = FirebaseAuth.instance.currentUser;
-
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -82,50 +84,10 @@ class HomeScreenContent extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Text(
               'Hoşgeldin, ${user?.displayName ?? 'Kullanıcı'}!',
-              style: const TextStyle(fontSize: 24),
+              style: const TextStyle(fontSize: 24, color: Colors.white),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _signOut(context),
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
-              child: const Text('Oturumu Kapat'),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black,
-        selectedItemColor: Colors.purple,
-        unselectedItemColor: Colors.white,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Ana Sayfa',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category),
-            label: 'Kategoriler',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favoriler',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Ara',
           ),
         ],
-        currentIndex: 0, // Ana Sayfa'nın seçili olmasını sağlamak için
-        onTap: (index) {
-          // Bottom Navigation Bar butonlarına tıklanınca yapılacak işlemleri buraya yazabilirsiniz.
-          if (index == 1) {
-            Navigator.pushNamed(context, '/categories');
-          } else if (index == 2) {
-            Navigator.pushNamed(context, '/favorites');
-          } else if (index == 3) {
-            Navigator.pushNamed(context, '/search');
-          }
-        },
       ),
     );
   }
